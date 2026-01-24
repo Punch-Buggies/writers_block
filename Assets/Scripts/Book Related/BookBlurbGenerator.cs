30fa66c7e1b24111896ccc83ab2bc65c6cabccea
meghan <144631876+meghanwickstrand@users.noreply.github.com>
2026-01-24 13:56:04 -0700

    created BookBlurbGenerator

---
+++ Assets/Scripts/Book Related/BookBlurbGenerator.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class BookBlurbGenerator : MonoBehaviour
+{
+    [SerializeField] string genre;
+    [SerializeField] string character;
+    [SerializeField] string setting;
+}
