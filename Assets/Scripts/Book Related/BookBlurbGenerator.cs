3e1b9820f03f261cbf915fefe49a79c5e1c3a203
meghan <144631876+meghanwickstrand@users.noreply.github.com>
2026-01-24 15:09:37 -0700

    restructure to use List<string>

--- Assets/Scripts/Book Related/BookBlurbGenerator.cs
+++ Assets/Scripts/Book Related/BookBlurbGenerator.cs
@@ -1,8 +1,22 @@
 using UnityEngine;
 
+
 public class BookBlurbGenerator : MonoBehaviour
 {
     [SerializeField] string genre;
     [SerializeField] string character;
     [SerializeField] string setting;
+
+    [SerializeField] BookBlurbSupplier bookBlurbSupplier;
+    
+    string sample_template = "{0} fell in love with a {1} {2}!";
+
+    void Start()
+    {
+        var femaleNames = bookBlurbSupplier.GetNames("f");
+        int idx = Random.Range(0, femaleNames.Count);
+        string charA = femaleNames[idx];
+        Debug.Log(string.Format(sample_template, charA, "B", "C"));
+    }
+    
 }
--- Assets/Scripts/Book Related/BookBlurbSupplier.cs
+++ Assets/Scripts/Book Related/BookBlurbSupplier.cs
@@ -8,81 +8,60 @@ public class BookBlurbSupplier : MonoBehaviour
 //      character_words = {key:storyelement = [[adjectives], [catchphrases]]}
 //      names = {key:gender = [names]}
 {
-    Dictionary<string, string[][]> setting_words;
-    Dictionary<string, string[][]> character_words;
-    Dictionary<string, string[]> names;
+    Dictionary<string, List<List<string>>> setting_words;
+    Dictionary<string, List<List<string>>> character_words;
+    Dictionary<string, List<string>> names;
 
     void Awake()
     // initialize dictionaries
     {
-        setting_words = new Dictionary<string, string[][]>();
-        character_words = new Dictionary<string, string[][]>();
-        names = new Dictionary<string, string[]>();
+        setting_words = new Dictionary<string, List<List<string>>>();
+        character_words = new Dictionary<string, List<List<string>>>();
+        names = new Dictionary<string, List<string>>();
 
         BuildSampleData();
     }
 
-    public string[] GetAdjectives(string key)
-    {
-        return character_words[key][0];   
-    }
+    public List<string> GetAdjectives(string key)   => character_words[key][0];
+    public List<string> GetCatchphrases(string key) => character_words[key][1];
 
-    public string[] GetCatchphrases(string key)
-    {
-        return character_words[key][1];
-    }
+    public List<string> GetPeople(string key) => setting_words[key][0];
+    public List<string> GetPlaces(string key) => setting_words[key][1];
+    public List<string> GetThings(string key) => setting_words[key][2];
 
-    public string[] GetPeople(string key)
-    {
-        return setting_words[key][0];
-    }
-
-    public string[] GetPlaces(string key)
-    {
-        return setting_words[key][1];
-    }
-
-    public string[] GetThings(string key)
-    {
-        return setting_words[key][2];
-    }
-
-    public string[] GetNames(string key)
-    {
-        return names[key];
-    }
+    public List<string> GetNames(string key) => names[key];
 
     void BuildSampleData()
     // sample data for testing purposes, actual data will link to csv
     {
-        setting_words.Add("Spaceship", new string[][]
+        setting_words.Add("Spaceship", new List<List<string>>
         {
-            new string[] { "alien", "captain", "stormtrooper" },
-            new string[] { "control room", "research lab", "med bay" },
-            new string[] { "lightsaber", "spacesuit", "beaker" }
+            new List<string> { "alien", "captain", "stormtrooper" },
+            new List<string> { "control room", "research lab", "med bay" },
+            new List<string> { "lightsaber", "spacesuit", "beaker" }
         });
 
-        setting_words.Add("Castle", new string[][]
+        setting_words.Add("Castle", new List<List<string>>
         {
-            new string[] { "king", "jester", "knight", "squire" },
-            new string[] { "throne room", "stable", "royal courtyard", "ballroom" },
-            new string[] { "crown", "jewel", "broadsword", "flag" }
+            new List<string> { "king", "jester", "knight", "squire" },
+            new List<string> { "throne room", "stable", "royal courtyard", "ballroom" },
+            new List<string> { "crown", "jewel", "broadsword", "flag" }
         });
 
-        character_words.Add("Villain", new string[][]
+        character_words.Add("Villain", new List<List<string>>
         {
-            new string[] { "evil", "somber", "piercing", "twisted" },
-            new string[] { "I will avenge my dead father!", "Pity. You're too late!" }
+            new List<string> { "evil", "somber", "piercing", "twisted" },
+            new List<string> { "I will avenge my dead father!", "Pity. You're too late!" }
         });
 
-        character_words.Add("Business_Person", new string[][]
+        character_words.Add("Business_Person", new List<List<string>>
         {
-            new string[] { "plain", "greedy", "bored", "soulless" },
-            new string[] { "Let me crunch the numbers...", "Have my secretary schedule that in for next week." }
+            new List<string> { "plain", "greedy", "bored", "soulless" },
+            new List<string> { "Let me crunch the numbers...", "Have my secretary schedule that in for next week." }
         });
 
-        names.Add("f", new string[] { "Mary", "Lottie", "Amelia", "Pauline", "Molly", "Harriet", "Leah", "Astrid" });
-        names.Add("m", new string[] { "Bob", "Reggie", "Reginald", "Barty", "John", "Maverick", "Nicholas", "Xavier" });
-        names.Add("nb", new string[] { "Alex", "Loren", "Avery", "Stardust", "Steel Lightning" });
+        names.Add("f", new List<string> { "Mary", "Lottie", "Amelia", "Pauline", "Molly", "Harriet", "Leah", "Astrid" });
+        names.Add("m", new List<string> { "Bob", "Reggie", "Reginald", "Barty", "John", "Maverick", "Nicholas", "Xavier" });
+        names.Add("nb", new List<string> { "Alex", "Loren", "Avery", "Stardust", "Steel Lightning" });
     }
 }
