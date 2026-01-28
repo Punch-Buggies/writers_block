32ba81ec2a1adc6a903e11652911b1aafed007e6
Maggie Lacson <58317354+orangejuic3box@users.noreply.github.com>
2026-01-27 17:34:28 -0700

    init book class commit

---
+++ Assets/Scripts/Book Related/Book.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System;
+using TMPro;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class Book : MonoBehaviour
+{
+    [SerializeField] string genre;
+    [SerializeField] string character;
+    [SerializeField] string setting;
+    [SerializeField] string copiesSold;
+    [SerializeField] string moneyMade;
+    [Serializefield] Boolean bestSelling;
+    
+}
