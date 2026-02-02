5ef07dd27ed1186d666baa5f0f7d5f6b9810df9e
Maggie Lacson <58317354+orangejuic3box@users.noreply.github.com>
2026-02-02 16:21:23 -0700

    playing with colors

--- Assets/Scripts/Thought Bubble/ThoughtBubbleHandler.cs
+++ Assets/Scripts/Thought Bubble/ThoughtBubbleHandler.cs
@@ -1,4 +1,7 @@
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThoughtBubbleHandler : MonoBehaviour
 {
@@ -63,6 +66,12 @@ public class ThoughtBubbleHandler : MonoBehaviour
             Quaternion.identity, 
             transform // Set as child of this GameObject
         );
+
+        UnityEngine.Color c = UnityEngine.Color.blue;
+        spawnedElement.GetComponent <Image>().color = c;
+        // actual image is whats it called in uniy editor whatevrr man'
+        spawnedElement.GetComponentsInChildren<Image>().color = c;
+;
         
 
         // Filling up the data for both the scripts
