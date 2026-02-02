34f1ca2203ac96c6dc3a3768c11ccb1e2a767cf2
FaiazBN <faiaz.nesar@gmail.com>
2026-02-02 15:17:36 -0700

    UI Blow Up!!!

--- Assets/Scripts/Story Elements/DraggableItem.cs
+++ Assets/Scripts/Story Elements/DraggableItem.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] GameObject BGImage;
     [SerializeField] Transform initialLocation;
     bool wasPlacedSuccessfully = false;
     private CanvasGroup canvasGroup;
+
+    [SerializeField] float scaleFactor = 2f;
+    [SerializeField] float scaleTime = 0.5f;
+
+    Vector2 initialScale;
 
     void Awake()
     {
@@ -18,6 +23,7 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
         {
             canvasGroup = gameObject.AddComponent<CanvasGroup>();
         }
+        initialScale = transform.localScale;
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -29,6 +35,7 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     {
         RectTransformUtility.ScreenPointToWorldPointInRectangle(transform.parent as RectTransform, eventData.position, eventData.pressEventCamera, out Vector3 globalMousePos);
         transform.position = globalMousePos;
+
         //transform.position = eventData.position;
     }
 
@@ -45,12 +52,21 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        BGImage.SetActive(true);
+        // Uncomment if you don't want to use DOTween and lerping
+        // Transform actualImageTransform = transform;
+        // Vector2 scaled = new Vector2(actualImageTransform.localScale.x * scaleFactor, 
+        //                             actualImageTransform.localScale.y * scaleFactor);
+
+        // actualImageTransform.localScale = scaled;
+
+        transform.DOScale(new Vector3(transform.localScale.x * scaleFactor, 
+        transform.localScale.y * scaleFactor, 
+        transform.localScale.z), scaleTime).SetEase(Ease.OutBounce);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        BGImage.SetActive(false);
+        transform.DOScale(initialScale, scaleTime).SetDelay(scaleTime/2).SetEase(Ease.OutBounce);
     }
 
     public void SetInitialLocation(Transform location)
@@ -68,6 +84,5 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     void Start()
     {
         transform.position = initialLocation.position;
-        BGImage.SetActive(false);
     }
 }
