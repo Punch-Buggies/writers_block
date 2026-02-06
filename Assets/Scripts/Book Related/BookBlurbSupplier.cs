d90103191a55bc89e395ded6e99eaeb20a9597b6
Maggie Lacson <58317354+orangejuic3box@users.noreply.github.com>
2026-02-06 12:36:41 -0700

    add in temporary templates

--- Assets/Scripts/Book Related/BookBlurbSupplier.cs
+++ Assets/Scripts/Book Related/BookBlurbSupplier.cs
@@ -251,6 +251,71 @@ public class BookBlurbSupplier : MonoBehaviour
                 break;
             }
         }
+        // adding templates for testing, remove this once the addTemplateData has been made
+        templates.Add("Test", new List<BookBlurbTemplate>
+        {
+            new BookBlurbTemplate(
+                "The {first_name} saw {their1} {thing_a}. Then, the {name2}, {they2} took it from {them1}",
+                new List<TemplateSlot>
+                {   
+                    new TemplateSlot("person1", WordType.Person),
+                    new TemplateSlot("their1", WordType.Pronoun, parentId:"person1", perspective:Perspective.PossessiveAdj),
+                    new TemplateSlot("thing_a", WordType.Thing),
+                    new TemplateSlot("person2", WordType.Person),
+                    new TemplateSlot("they2", WordType.Pronoun, parentId:"person2", perspective:Perspective.Subject),
+                    new TemplateSlot("them1", WordType.Pronoun, parentId:"person1", perspective:Perspective.Object),
+                    new TemplateSlot("first_name", WordType.Name, parentId:"person1"),
+                    new TemplateSlot("name2", WordType.Name, parentId:"person2")
+                },
+                "Test"
+            )
+            
+        });
+        templates.Add("Romance", new List<BookBlurbTemplate>
+        {
+            new BookBlurbTemplate(
+                "{name1} kissed the {adjective1} {person1} in the {place1}. But, the {person1} was actually {a} {person2}!",
+                new List<TemplateSlot>
+                {
+                    new TemplateSlot("name1", WordType.Name, parentId:"character"),
+                    new TemplateSlot("adjective1", WordType.Adjective),
+                    new TemplateSlot("person1", WordType.Person),
+                    new TemplateSlot("person2", WordType.Person),
+                    new TemplateSlot("place1", WordType.Place),
+                    new TemplateSlot("a", WordType.IndefiniteArticle, parentId:"person2")
+                },
+                "Romance"
+            ),
+            new BookBlurbTemplate(
+                "Holding the {thing1} between {his_posses} teeth in a {adjective1} bite, paying careful attention to the {thing2}, {name1}’s hands were now free to wander down {his_posses} lap where {he} quickly got to work unzipping {him}self.",
+                new List<TemplateSlot>
+                {
+                    new TemplateSlot("thing1", WordType.Thing),
+                    new TemplateSlot("adjective1", WordType.Adjective),
+                    new TemplateSlot("thing2", WordType.Thing),
+                    new TemplateSlot("name1", WordType.Name, parentId:"character"),
+                    new TemplateSlot("his", WordType.Pronoun, parentId:"character", perspective:Perspective.PossessivePro),
+                    new TemplateSlot("he", WordType.Pronoun, parentId:"character", perspective:Perspective.Subject),
+                    new TemplateSlot("him", WordType.Pronoun, parentId:"character", perspective:Perspective.Object),
+                    new TemplateSlot("his_posses", WordType.Pronoun, parentId:"character", perspective:Perspective.PossessiveAdj)
+                },
+                "Romance"
+            ),
+            new BookBlurbTemplate(
+                "{name1} surged forward, catching {name2}'s lips in {a1} {adjective1} kiss. The {person1}'s lips parted formed a surprised 'o'. {name1} slid {their} tongue forward, watching {name2}'s eyes curiously.",
+                new List<TemplateSlot>
+                {
+                    new TemplateSlot("name1", WordType.Name, parentId:"character"),
+                    new TemplateSlot("name2", WordType.Name, parentId:"person1"),
+                    new TemplateSlot("adjective1", WordType.Adjective),
+                    new TemplateSlot("person1", WordType.Person),
+                    new TemplateSlot("a1", WordType.IndefiniteArticle, parentId:"adjective1"),
+                    new TemplateSlot("their", WordType.Pronoun, parentId:"character", perspective:Perspective.PossessiveAdj)
+            
+                },
+                "Romance"
+            ),
+        });
 
     }
 // end of BookBLurnSupplier Class
