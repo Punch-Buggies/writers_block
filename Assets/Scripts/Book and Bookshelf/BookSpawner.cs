cf7aabd68680d767e4735ae0ec816339bb6ee5b2
Maggie Lacson <58317354+orangejuic3box@users.noreply.github.com>
2026-02-22 17:55:13 -0700

    delete inside spawner

--- Assets/Scripts/Book and Bookshelf/BookSpawner.cs
+++ Assets/Scripts/Book and Bookshelf/BookSpawner.cs
@@ -2,31 +2,31 @@ using UnityEngine;
 
 public class BookInsideSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject bookInsidePrefab;
+    // [SerializeField] private GameObject bookInsidePrefab;
     [SerializeField] private GameObject bookCoverPrefab;
     [SerializeField] private Transform parentContainer;
 
+    // [SerializeField] private Transform bookViewContainer;
+    
 
+    // public void SpawnInside(string blurb )
+    // {
+    //     // spawn inside
+    //     GameObject newInside = Instantiate(bookInsidePrefab, bookViewContainer);
 
-    public void SpawnInside(string blurb )
-    {
-        // spawn inside
-        GameObject newInside = Instantiate(bookInsidePrefab, parentContainer);
+    //     BookInside insideScript = newInside.GetComponent<BookInside>();
+    //     insideScript.Initialize(blurb);
 
-        BookInside insideScript = newInside.GetComponent<BookInside>();
-        insideScript.Initialize(blurb);
+    // }
 
-
-    }
-
-    public void SpawnCover(string title, bool bestseller, int copiesSold)
+    public void SpawnCover(string title, bool bestseller, int copiesSold, string blurb)
     {
         // spawn outside
         GameObject newCover = Instantiate(bookCoverPrefab, parentContainer);
         BookCover coverScript = newCover.GetComponent<BookCover>();
         string bestsellerStr = bestseller ? "Bestseller" : "";;
         string coverHeaderStr = $"{copiesSold}\ncopies sold\n{bestsellerStr}";
-        coverScript.Initialize(title, coverHeaderStr);
+        coverScript.Initialize(title, coverHeaderStr, blurb);
     }
 
 }
