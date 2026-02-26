9a21c09501a990de51ac91d0b55a9f28830ed10e
Maggie Lacson <58317354+orangejuic3box@users.noreply.github.com>
2026-02-25 19:00:06 -0700

    audio manager update

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour
 {
@@ -12,8 +13,11 @@ public class AudioManager : MonoBehaviour
     [Range(0f, 1f)] public float musicVolume = 1f;
     [Range(0f, 1f)] public float sfxVolume = 1f;
 
+    [Header("Music")]
+    [SerializeField] public AudioClip mainMusic; 
+
     // cache for audio clips
-    private Dictionary<string, AudioCLip> clipCache = new Dictionary<string, AudioClip>();
+    private Dictionary<string, AudioClip> clipCache = new Dictionary<string, AudioClip>();
 
     private void Awake()
     {
@@ -33,15 +37,33 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
+        Debug.Log("audio manager starting up");
         UpdateVolume();
+        PlayMusic(mainMusic);
+        Debug.Log("main music is playing now");
     }
+
+
+    // BG MUSIC
+    public void PlayMusic(AudioClip clip)
+    {
+        if (clip == null) return;
+        if (musicSource.clip == clip && musicSource.isPlaying) return; // already playing
+
+        musicSource.clip = clip;
+        musicSource.loop = true;
+        musicSource.volume = 1f;
+        musicSource.Play();
+    }
+
 
     // RANDOMIZER
     public void PlayUniqueBookSound(string genre, string character, string setting)
     {
         PlaySound("Audio/Randomizer/GENRE/GENRE_" + genre);
-        PlaySound("Audio/Randomizer/CHARACTER/CHARACTER_" + genre);
-        PlaySound("Audio/Randomizer/SETTING/SETTING_" + genre);
+        PlaySound("Audio/Randomizer/CHARACTER/CHARACTER_" + character);
+        PlaySound("Audio/Randomizer/SETTING/SETTING_" + setting);
+        Debug.Log("played all the sounds");
     }
 
     private void PlaySound(string path)
@@ -57,12 +79,25 @@ public class AudioManager : MonoBehaviour
             }
             else
             {
-                Debug.LogWarning"Audio not found at: " + path);
+                Debug.LogWarning("Audio not found at: " + path);
                 return;
             }
         }
 
+        if (clip == null)
+        {
+            Debug.LogWarning("clip is null at: " + path);
+        }
+
+        Debug.Log("Playing clip: " + path);
         sfxSource.PlayOneShot(clip);
+    }
+
+    public void playTest()
+    {
+        // PlayUniqueBookSound("Romance","Antihero","Mountains");
+        PlaySound("Audio/Randomizer/CHARACTER/CHARACTER_Magician");
+        Debug.Log("test over");
     }
 
     // VOLUME
@@ -70,13 +105,13 @@ public class AudioManager : MonoBehaviour
     public void SetMusicVolume(float volume)
     {
         musicVolume = volume;
-        UpdateVolumes();
+        UpdateVolume();
     }
 
     public void SetSFXVolume(float volume)
     {
         sfxVolume = volume;
-        UpdateVolumes();
+        UpdateVolume();
     }
 
     private void UpdateVolume()
