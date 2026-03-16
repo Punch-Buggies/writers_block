d2d22bcb33582991e820af22d4674b0300706548
Maggie Lacson <58317354+orangejuic3box@users.noreply.github.com>
2026-03-16 16:04:52 -0600

    set color to a random setting color

--- Assets/Scripts/Book and Bookshelf/BookCover.cs
+++ Assets/Scripts/Book and Bookshelf/BookCover.cs
@@ -2,25 +2,39 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using Color = UnityEngine.Color;
+
 
 public class BookCover : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI titleText;
     [SerializeField] private TextMeshProUGUI cornerText;
+    // randomize, randomize
+    [SerializeField] private Image bg_image;
+    [SerializeField] private Image outer_border;
+    [SerializeField] private Image inner_border;
 
     // title to display in prefab
     private string title;
     // corner header string
     private string cornerHeader;
     private Book book;
-
+    Dictionary<string, Color[]> colorDict;
+    
+    
     public void Initialize(string title, string cornerHeader, Book bookData)
     {
 
         titleText.text = title;
         cornerText.text = cornerHeader;
         book = bookData;
+        
+        MakeColorDic();
 
+        // create book cover image according to its attribute
+        SetColors(book);
+        //TODO: set images
     }
 
     // add button for opening bookview
@@ -30,4 +44,51 @@ public class BookCover : MonoBehaviour
         BookViewManager.Instance.OpenBookView(book);
     }
 
+    // the Hex() function is used when intiializing the colorDict to convert them straight to type Color
+    private Color Hex(string hexcode)
+    {
+        Color color;
+        ColorUtility.TryParseHtmlString(hexcode, out color);
+        return color;
+    }
+
+    private Color RandomlyPickAColor(string setting)
+    {
+        Debug.Log($"Picking a color for {setting}");
+        //find it in the dictionary, randomly pick one of the colors
+        Color[] colors = colorDict[setting];
+        Debug.Log($"colors is {colors.Length} long");
+        Color randomColor = colors[Random.Range(0, colors.Length)];
+        return randomColor;
+    }
+
+    private void SetColors(Book book)
+    {
+        // randomly selects the colors for bg and borders
+        string setting = book.setting;
+        Debug.Log($"please tell me we got the setting {setting}");
+        bg_image.color = RandomlyPickAColor(setting);
+        outer_border.color = RandomlyPickAColor(setting);
+        inner_border.color = RandomlyPickAColor(setting);
+    }
+
+///////////// color dictionary //////////////
+    private void MakeColorDic()
+    {
+        colorDict = new Dictionary<string, Color[]>
+        {
+            // uses the helper Hex function to convert hexcode string to a Color object
+            { "OuterSpace", new Color[] { Hex("#0B1026"), Hex("#2E0854"), Hex("#00D9FF") } },
+            { "Office", new Color[] { Hex("#D1D5DB"), Hex("#F3E5AB"), Hex("#1F2937") } },
+            { "Pre-historic", new Color[] { Hex("#5C4033"), Hex("#4B5320"), Hex("#E3DAC9") } },
+            { "Mountains", new Color[] { Hex("#708090"), Hex("#F0F8FF"), Hex("#2D5A27") } },
+            { "Forest", new Color[] { Hex("#223311"), Hex("#889933"), Hex("#4B3621") } },
+            { "Underwater", new Color[] { Hex("#001219"), Hex("#00AFB9"), Hex("#FCFFCB") } },
+            { "WildWest", new Color[] { Hex("#C27E3A"), Hex("#4A3728"), Hex("#7B3F40") } },
+            { "Medieval", new Color[] { Hex("#7A7D7D"), Hex("#AD9F7C"), Hex("#8B0000") } },
+            { "Campus", new Color[] { Hex("#A64439"), Hex("#F9C74F"), Hex("#264653") } },
+            { "Hospital", new Color[] { Hex("#E0F2F1"), Hex("#008080"), Hex("#D32F2F") } },
+            { "Basement", new Color[] { Hex("#4B4B4B"), Hex("#8B5A2B"), Hex("#FFBF00") } }
+        };
+    }
 }
